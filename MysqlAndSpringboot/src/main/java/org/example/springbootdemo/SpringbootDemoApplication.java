69575f620cb1ae03d99aff8fcf94b40b7fa35b77
jiangyuhuai <2095361732@qq.com>
2025-05-19 18:55:53 +0800

    update some mistakes

--- MysqlAndSpringboot/src/main/java/org/example/springbootdemo/SpringbootDemoApplication.java
+++ MysqlAndSpringboot/src/main/java/org/example/springbootdemo/SpringbootDemoApplication.java
@@ -1,11 +1,13 @@
 package org.example.springbootdemo;
 
+import org.apache.catalina.util.ServerInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootDemoApplication {
     public static void main(String[] args) {
+        System.out.println("Tomcat version: " + ServerInfo.getServerInfo());
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
 }
